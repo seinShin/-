f6a721cc26f57de8ba93a18cb9800de3fc47efa9
seinShin <tlstpdls13@gmail.com>
2024-01-31 17:24:08 +0900

    Feat : 폴더 구조 설정 및 로그인 페이지 레이아웃 세팅

--- interviewRecipe/src/main/java/interviewRecipe/interviewRecipe/InterviewRecipeApplication.java
+++ interviewRecipe/src/main/java/interviewRecipe/interviewRecipe/InterviewRecipeApplication.java
@@ -2,10 +2,11 @@ package interviewRecipe.interviewRecipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class InterviewRecipeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(InterviewRecipeApplication.class, args);
 	}
