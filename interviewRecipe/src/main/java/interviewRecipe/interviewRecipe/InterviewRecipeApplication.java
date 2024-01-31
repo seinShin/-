37824858ff0c6e01d64356f0176c5c1946b2393a
seinShin <tlstpdls13@gmail.com>
2024-01-31 16:14:44 +0900

    Feat : 프로젝트 환경 세팅

---
+++ interviewRecipe/src/main/java/interviewRecipe/interviewRecipe/InterviewRecipeApplication.java
@@ -0,0 +1,13 @@
+package interviewRecipe.interviewRecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewRecipeApplication.class, args);
+	}
+
+}
---
+++ interviewRecipe/src/test/java/interviewRecipe/interviewRecipe/InterviewRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package interviewRecipe.interviewRecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
