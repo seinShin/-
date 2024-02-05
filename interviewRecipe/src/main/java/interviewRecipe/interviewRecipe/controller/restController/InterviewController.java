234ceaeb316503523d32d1d62af3d58463ee5444
seinShin <tlstpdls13@gmail.com>
2024-02-05 14:27:52 +0900

    Refac : 면접 주제 컨트롤러 Mapping 수정

--- interviewRecipe/src/main/java/interviewRecipe/interviewRecipe/controller/restController/InterviewController.java
+++ interviewRecipe/src/main/java/interviewRecipe/interviewRecipe/controller/restController/InterviewController.java
@@ -12,21 +12,21 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/interview")
+@RequestMapping("/v1/interview")
 @AllArgsConstructor
 public class InterviewController {
 
     private final InterviewService interviewService;
 
     //주제 등록
-    @PostMapping("/create")
+    @PostMapping("/")
     public ResponseEntity<Integer> createInterview(InterviewDto interviewDto){
         int rst = interviewService.save(interviewDto);
         return ResponseEntity.ok(rst);
     }
 
     //주제 전체 조회
-    @GetMapping("/listAll")
+    @GetMapping("/")
     public ResponseEntity<List<InterviewDto>> interviewList(Model model){
 //        List<InterviewDto> interviewList = interviewService.interviewDtoList();
 //        model.addAttribute("interviews", interviewList);
@@ -34,7 +34,7 @@ public class InterviewController {
 
     }
 
-    //주제 상세 조회
+    //주제 상세 조회 + 해당 질문 조회
     @GetMapping("/{titleId}")
     public ResponseEntity<InterviewDto> interviewById(@PathVariable int titleId, Model model){
 //        InterviewDto interviewDto = interviewService.getInterview((long) titleId);
@@ -43,9 +43,9 @@ public class InterviewController {
     }
 
     //주제 수정
-    @PutMapping("/{titleId}")
-    public ResponseEntity<Integer> interviewUpdate(@PathVariable int titleId, InterviewDto interviewDto){
-        InterviewDto interview = interviewService.getInterview((long)titleId);
+    @PutMapping("/")
+    public ResponseEntity<Integer> interviewUpdate(InterviewDto interviewDto){
+        InterviewDto interview = interviewService.getInterview(interviewDto.getTitleId());
         interview.builder()
                 .title(interviewDto.getTitle())
                 .updDt(LocalDateTime.now())
@@ -54,7 +54,7 @@ public class InterviewController {
     }
 
     //주제 삭제
-    @DeleteMapping("/delete/{titleId}")
+    @DeleteMapping("/{titleId}")
     public ResponseEntity<Integer> interviewDelete(@PathVariable int titleId){
         return ResponseEntity.ok(interviewService.delete((long) titleId));
     }
