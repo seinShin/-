9f6c92d699f2187f18ba2d7ebe54b567be3159f9
seinShin <tlstpdls13@gmail.com>
2024-02-06 10:39:25 +0900

    Fix : Type Alias 안먹는 문제 해결 -> 모든 파일 경로 처음부터 설정

--- interviewRecipe/src/main/java/interviewRecipe/interviewRecipe/dto/InterviewDto.java
+++ interviewRecipe/src/main/java/interviewRecipe/interviewRecipe/dto/InterviewDto.java
@@ -1,9 +1,6 @@
 package interviewRecipe.interviewRecipe.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
