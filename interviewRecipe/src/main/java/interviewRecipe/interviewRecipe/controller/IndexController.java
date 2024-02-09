a8dda4a728bf0036759c7775a216af5beedaee18
seinShin <tlstpdls13@gmail.com>
2024-02-09 20:13:37 +0900

    Refac : index 컨트롤러 리팩토링

--- interviewRecipe/src/main/java/interviewRecipe/interviewRecipe/controller/IndexController.java
+++ interviewRecipe/src/main/java/interviewRecipe/interviewRecipe/controller/IndexController.java
@@ -1,5 +1,7 @@
 package interviewRecipe.interviewRecipe.controller;
 
+import interviewRecipe.interviewRecipe.dto.MemberDto;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +10,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class IndexController {
 
-    // 회원 로그인
-    @GetMapping("/main")
-    public String main(){
-        return "/main";
+    /* 메인 페이지
+     * 로그인 성공시 -> 메인 페이지
+     * */
+    @GetMapping("main")
+    public String loginToMain(){
+        return "main";
     }
+
+
+    /* 기본 로그인 페이지
+     *
+     * */
+    @GetMapping("")
+    public String login( ){
+
+        return "index";
+    }
+
+
+    @GetMapping("join")
+    public String joinPage(){
+        System.out.println("123123");
+        return "/member/join";
+    }
+
+
+    @GetMapping("interview")
+    public String interviewTest(){
+        System.out.println("123123");
+        return "/interview/interview";
+    }
+
 
     // 메인 페이지
 
